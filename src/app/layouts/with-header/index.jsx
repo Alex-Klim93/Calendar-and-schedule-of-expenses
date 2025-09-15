05a8afacf90432487266feaf3b065e93ad144a6f
44SHADE <xorolich@mail.ru>
2025-09-15 22:16:14 +0300

    Добавлен весь интерфейс страницы расходов

--- src/app/layouts/with-header/index.jsx
+++ src/app/layouts/with-header/index.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router-dom';
 import { Header } from '@/shared/ui/header';
 
+import styles from './styles.module.css';
+
 export const Layout = () => {
   return (
-    <div className="layout">
+    <div className={styles.layout}>
       <Header isMain={true} />
       <main className="main-content">
         <Outlet />
