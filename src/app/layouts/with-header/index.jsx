bd858d5c100e529c301435e2e01e1033ce1c15d1
Alex-Klim93 <melkimelki79@gmail.com>
2025-09-29 15:45:08 +0300

    temp: save current work before switching

--- src/app/layouts/with-header/index.jsx
+++ src/app/layouts/with-header/index.jsx
@@ -1,9 +1,9 @@
 import { Outlet } from 'react-router-dom';
-import { Header } from '@/shared/ui/header';
+import { Header } from '@/shared/ui/header 2';
 
 export const Layout = () => {
   return (
-    <div className="layout">
+    <div>
       <Header isMain={true} />
       <main className="main-content">
         <Outlet />
